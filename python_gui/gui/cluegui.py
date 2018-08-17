103c6b8769721e822d6bf07dd906b55ee6a20232
Vincent <themilkmentaskforce@gmail.com>
2018-08-17 14:38:17 -0400

    More details in the GUI

--- python_gui/gui/cluegui.py
+++ python_gui/gui/cluegui.py
@@ -6,34 +6,65 @@ from PIL import Image, ImageTk
 from python_gui.gui.tristatebutton import TriStateButton
 from python_gui.constants import people, weapons, rooms
 from python_gui.gui.listchoice import ListChoice
+from python_gui.gui.combolabel import ComboLabel
+from python_gui.gui.resources import *
 
 
 class ClueGui(ttk.Frame):
     def __init__(self, *args, **kwargs):
         ttk.Frame.__init__(self, *args, **kwargs)
         self.left = ttk.Frame(self)
-        self.right = ListChoice(self, width=100)
-        self.people, self.people_buttons = self.create_button_grid(people, 4)
-        self.rooms, self.room_buttons = self.create_button_grid(rooms, 4)
-        self.weapons, self.weapon_buttons = self.create_button_grid(weapons, 4)
+        self.right = ttk.Frame(self)
+        self.choices = ListChoice(self.right, width=100)
+        self.guesses = self.create_guess_selector()
+        self.images = []
+        self.people, self.people_buttons = self.create_button_grid(4, people, [str(i + 1) for i in range(4)])
+        self.weapons, self.weapon_buttons = self.create_button_grid(4, weapons)
+        self.rooms, self.room_buttons = self.create_button_grid(4, rooms)
 
         self.left.pack(side=tk.LEFT)
         self.people.pack(pady=(0, 5))
-        self.rooms.pack(pady=5)
         self.weapons.pack(pady=(5, 0))
-        self.right.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.BOTH, padx=(5, 0), pady=(5, 0))
+        self.rooms.pack(pady=5)
+        self.right.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.BOTH, padx=(5, 10), pady=(5, 0))
+        self.choices.pack(side=tk.LEFT, expand=tk.YES, fill=tk.BOTH)
+        self.guesses.pack(side=tk.LEFT, fill=tk.Y)
 
-    def create_button_grid(self, row_names, columns):
-        buttons = []
+    def create_button_grid(self, columns, row_names, column_names=list()):
+        buttons = [[] for row in range(len(row_names))]
         frame = ttk.Frame(self.left)
         for i, name in enumerate(row_names):
-            buttons.append([])
-            row_frame = ttk.Frame(frame)
-            row_frame.pack()
-            ttk.Label(row_frame, text=name, width=15, anchor=tk.E).pack(side=tk.LEFT)
-            for j in range(columns):
-                button = TriStateButton(row_frame)
+            ttk.Label(frame, text=name, width=15, anchor=tk.E).grid(row=i+2, column=1)
+        for j, name in enumerate(column_names):
+            ttk.Label(frame, text=name, style='Subtitle.TLabel').grid(row=1, column=j+2)
+        for j in range(columns):
+            for i in range(len(row_names)):
+                button = TriStateButton(frame)
                 button.config(command=lambda b=button: b.toggle())
+                button.grid(row=i+2, column=j+2)
                 buttons[i].append(button)
-                button.pack(side=tk.LEFT)
         return frame, buttons
+
+    def create_guess_selector(self):
+        frame = ttk.Frame(self.right)
+        label = ttk.Label(frame, text='Make Suggestion', style='Subtitle.TLabel')
+        guessor = ComboLabel(frame, text='Guessing Player?')
+        character = ComboLabel(frame, text='Murderer?')
+        weapon = ComboLabel(frame, text='Weapon?')
+        location = ComboLabel(frame, text='Room?')
+        answerer = ComboLabel(frame, text='Answering Player?')
+        shown = ComboLabel(frame, text='Card Shown?')
+        confirm = ttk.Button(frame, text='Confirm')
+
+        shown.state(['disabled'])
+
+        label.pack()
+        guessor.pack()
+        character.pack()
+        weapon.pack()
+        location.pack()
+        answerer.pack()
+        shown.pack()
+        confirm.pack(pady=10)
+
+        return frame
--- python_gui/main.py
+++ python_gui/main.py
@@ -2,10 +2,12 @@ from tkinter import ttk
 import tkinter as tk
 
 from python_gui.gui.cluegui import ClueGui
+from python_gui.gui.styles import configure_styles
 
 
 def main():
     root = tk.Tk()
+    configure_styles()
     gui = ClueGui(root)
     gui.pack(expand=tk.YES, fill=tk.BOTH)
     root.mainloop()
